b31e8a4d11a59e854651f5d7598a0a0c0b772789
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-05-30 10:45:19 -0400

    added loading function to weather icon

--- scripts/main.js
+++ scripts/main.js
@@ -5,26 +5,25 @@
 
 const hello = document.querySelector("header > h1");
 const weatherContainer = document.querySelector(".weather");
+const h2 = document.createElement("h2");
+const h3 = document.createElement("h3");
 
 const fetchWeather = () => {
     fetch(`https://flexweather.com/api/now?lat=40.650002&lon=-73.949997&units=imperial`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        console.log(data.temperature_current);
-        const h2 = document.createElement("h2");
-        const h3 = document.createElement("h3");
         const weatherSplit = data.condition.split("_")
         const weatherCondition = weatherSplit.join(" ")
+
         h2.innerText = data.temperature_current;
         h3.innerHTML = weatherCondition
-        weatherContainer.appendChild(h2);
-        weatherContainer.appendChild(h3);
-        console.log(weatherCondition);
-        if (weatherCondition.includes("cloudy")) {
+
+        if (weatherCondition) {
+            weatherContainer.innerText = ""
+            weatherContainer.appendChild(h2);
+            weatherContainer.appendChild(h3);
             weatherContainer.classList.remove("weather");
-            weatherContainer.classList.add("cloudy");
-            
+            weatherContainer.classList.add("cloudy");    
         }
     })
 }
@@ -39,5 +38,11 @@ const changeHeader = () => {
     })
 };
 
+const loading = () => {
+    h2.innerText = "Loading..."
+    weatherContainer.append(h2);
+};
+
+loading();
 changeHeader();
 fetchWeather();
